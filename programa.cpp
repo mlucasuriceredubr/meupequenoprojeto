5dbccd072019a2e9debfb8f1aefd0bf6c0c3ef2b
mlucasuriceredubr <72633771+mlucasuriceredubr@users.noreply.github.com>
2020-10-10 01:10:09 +0000

    correcao estetica

--- programa.cpp
+++ programa.cpp
@@ -4,13 +4,13 @@ using namespace std;
 // esta funcao recebe dois parametros e retorna um inteiro
 int funcao4(int a, int b)
 {
-	int x = (a+b)*(a-b);
+	int x =  ( a + b ) * ( a - b );
 	return x;
 }
 
 int main()
 {
   // Mostra uma mensagem de ola mundo
-  cout << "ola mundo." << endl;
+  cout << "Ola mundo." << endl;
   return 0;
 }
